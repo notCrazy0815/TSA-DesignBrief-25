b3eaca6ea4be2d8724f518d60b72a70576d0bd1d
notCrazy0815 <79633225+notCrazy0815@users.noreply.github.com>
2025-02-03 08:04:51 +0100

    basic scss structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+    export let title = "DESIGNBRIEF 25";
+</script>
+
+<main>
+    <header>
+        <h1>{title}</h1>
+    </header>
+    <section>
+        <slot></slot>
+    </section>
+    <footer>
+        <p>Footer</p>
+    </footer>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style lang="scss">
+     @use "../lib/styles/variables" as v;
+</style>
