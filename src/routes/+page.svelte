4def634cfb3dd76f76636a2d87e40a4b8e8598c6
notCrazy0815 <79633225+notCrazy0815@users.noreply.github.com>
2025-04-03 22:47:09 +0200

    window implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,10 @@
      import { page } from "$app/stores";
      import HeroCta from "$lib/components/HeroCta.svelte";
 
-     let keyAspectsSection: HTMLElement;
-     let isAnimating = false;
-     let scrollEnabled = false;
-     let activeSection = 0;
+     let plantsOnCard = false;
+     let card: HTMLElement;
+     let cardBg: HTMLElement;
+     let heroPlantsWrapper: HTMLElement;
 
      onMount(() => {
           window.scrollTo(0, 0);
@@ -17,69 +17,33 @@
                document.body.style.overflow = "hidden";
                setTimeout(() => {
                     document.body.style.overflow = "auto";
-                    scrollEnabled = true;
                }, 3000);
           }
+
+          sizeElements();
      });
 
-     let lastScrollY = 0;
-     let scrollDirection = 0;
-
-     function detectScrollDirection(event: WheelEvent | TouchEvent) {
-          let currentScrollY = window.scrollY;
-
-          if (event instanceof WheelEvent) {
-               scrollDirection = event.deltaY > 0 ? 1 : -1;
-          } else if (event instanceof TouchEvent) {
-               if (event.touches.length > 0) {
-                    let touchY = event.touches[0].clientY;
-                    scrollDirection = touchY < lastScrollY ? 1 : -1;
-                    lastScrollY = touchY;
-               }
+     const handleScroll = () => {
+          if (card.getBoundingClientRect().bottom <= window.innerHeight) {
+               plantsOnCard = true;
+          } else {
+               plantsOnCard = false;
           }
+     };
 
-          lastScrollY = currentScrollY;
+     const handleResize = () => {
+          sizeElements();
+     };
 
-          // if (scrollDirection === -1) {
-          //      keyAspectsSection.style.transform = "translateY(-100vh)";
-          // }
-     }
-
-     function handleScroll() {
-          if (!scrollEnabled || isAnimating) return;
-
-          const scrollPosition = window.scrollY;
-
-          if (activeSection === 0 && scrollPosition >= 1) {
-               isAnimating = true;
-               document.body.style.overflow = "hidden";
-               
-               activeSection = 1;
-
-               keyAspectsSection.style.transition = "transform 0.8s ease-in-out";
-               keyAspectsSection.style.transform = `translateY(${-window.innerHeight + scrollPosition}px)`;
-
-               setTimeout(() => {
-                    keyAspectsSection.style.position = "fixed";
-                    keyAspectsSection.style.top = "0";
-                    keyAspectsSection.style.transition = "none";
-                    keyAspectsSection.style.transform = "translateY(0)";
-                    isAnimating = false;
-               }, 800);
-          }
-
-          if (activeSection === 1) {
-               isAnimating = true;
-               document.body.style.overflow = "hidden";
-
-               activeSection = 0;
-               
-          }
-
+     const sizeElements = () => {
+          cardBg.style.width = `${heroPlantsWrapper.clientWidth}px`;
+          cardBg.style.height = `${heroPlantsWrapper.clientHeight}px`;
+          card.style.width = `${heroPlantsWrapper.clientWidth}px`;
+          card.style.height = `${heroPlantsWrapper.clientHeight}px`;
      }
 </script>
 
-<svelte:window on:scroll={handleScroll} on:wheel={detectScrollDirection} on:touchmove={detectScrollDirection} />
+<svelte:window on:scroll={handleScroll} on:resize={handleResize} />
 
 <NavBar />
 
@@ -87,17 +51,25 @@
      <section class="hero-section">
           <HeroTitle />
      </section>
-     <div class="hero-plants-container">
-          <HeroPlants />
+     <div class="hero-plants-container" class:plants-on-card={plantsOnCard}>
+          <div class="hero-plants-wrapper" bind:this={heroPlantsWrapper}>
+               <HeroPlants />
+          </div>
      </div>
      <div class="hero-cta-container">
           <HeroCta />
      </div>
-     <div class="key-aspects-wrapper">
-          <section class="key-aspects-section" bind:this={keyAspectsSection}>
-               <h2>Key Aspects</h2>
-          </section>
-     </div>
+     
+     <section class="scrollable">
+          <div class="scrollable-content">
+               <div class="card" bind:this={card}>
+                    <div class="card-content" class:plants-on-card={plantsOnCard}>
+                         <HeroPlants />
+                    </div>
+               </div>
+               <div class="card-bg" bind:this={cardBg}></div>
+          </div>
+     </section>
 </div>
 
 <style lang="scss">
@@ -114,16 +86,26 @@
      }
 
      .hero-plants-container {
-          display: flex;
-          justify-content: center;
-          align-items: end;
-
-          position: absolute;
+          position: fixed;
           width: 100%;
           height: 100%;
           top: 0;
           left: 0;
-          z-index: -1;
+          z-index: 1;
+
+          .hero-plants-wrapper {
+               width: clamp(350px, 90%, 1100px);
+               height: fit-content;
+
+               position: fixed;
+               bottom: 0;
+               left: 50%;
+               transform: translateX(-45%);
+          }
+
+          &.plants-on-card {
+               opacity: 0;
+          }
      }
 
      .hero-cta-container {
@@ -136,7 +118,7 @@
           justify-content: center;
           align-items: end;
           padding-bottom: 4rem;
-          z-index: 2;
+          z-index: 3;
      }
 
      @keyframes moveIn {
@@ -148,18 +130,51 @@
           }
      }
 
-     .key-aspects-wrapper {
+     .scrollable {
           position: absolute;
-          top: 100vh;
-          height: 100vh;
+          top: 100%;
           width: 100%;
-     }
+          height: 100vh;
 
-     .key-aspects-section {
-          position: absolute;
-          width: 100%;
-          height: 100vh;
-          background-color: v.$background-color-dark;
-          will-change: transform;
+          .scrollable-content {
+               width: 100%;
+               height: fit-content;
+               display: flex;
+               justify-content: center;
+               align-items: center;
+
+               .card {
+                    width: clamp(350px, 90%, 900px);
+                    aspect-ratio: 3/2;
+                    border-radius: 1rem;
+                    border: 5px solid #fff;
+                    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+                    z-index: 2;
+
+                    .card-content {
+                         width: 100%;
+                         height: 100%;
+                         display: flex;
+                         justify-content: center;
+                         align-items: end;
+                         opacity: 0;
+
+                         &.plants-on-card {
+                              opacity: 1;
+                         }
+                    }
+               }
+
+               .card-bg {
+                    position: absolute;
+                    top: 0;
+                    left: 50%;
+                    transform: translateX(-50%);
+                    background-color: v.$quaternary;
+                    border: 5px solid v.$quaternary;
+                    border-radius: 1rem;
+                    z-index: -1;
+               }
+          }
      }
 </style>
