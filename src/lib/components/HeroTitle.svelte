fcf54714f6d15965ac620b81fe66513fc5a4d274
notCrazy0815 <79633225+notCrazy0815@users.noreply.github.com>
2025-05-16 22:13:48 +0200

    scroll down hint

--- src/lib/components/HeroTitle.svelte
+++ src/lib/components/HeroTitle.svelte
@@ -42,20 +42,34 @@
             duration: 0.9,
             ease: "power2.inOut"
         }, "+=0.1");
+
+        gsap.fromTo(".hero-title-container p", {
+            opacity: 0,
+            y: -150
+        }, {
+            opacity: 1,
+            y: -150,
+            duration: 0.2,
+            ease: "power2.out",
+            delay: 2.5
+        })
     });
 </script>
 
-<h1 bind:this={heroTitleElement}>
-    {#each letters as letter, i}
-        {#if letter.isNewline}
-            <br />
-        {:else if letter.isSpace}
-            <span class="space">&nbsp;</span>
-        {:else}
-            <span class="letter">{letter.char}</span>
-        {/if}
-    {/each}
-</h1>
+<div class="hero-title-container">
+    <h1 bind:this={heroTitleElement}>
+        {#each letters as letter, i}
+            {#if letter.isNewline}
+                <br />
+            {:else if letter.isSpace}
+                <span class="space">&nbsp;</span>
+            {:else}
+                <span class="letter">{letter.char}</span>
+            {/if}
+        {/each}
+    </h1>
+    <p>Scroll down</p>
+</div>
 
 <style lang="scss">
     @use "../styles/variables" as v;
@@ -86,5 +100,14 @@
         width: 100%;
         content: "";
         display: block;
+    }
+
+    p {
+        font-size: clamp(0.8rem, 1.2vw, 1rem);
+        color: v.$font-color-dark;
+        opacity: 0;
+        transform: translateY(50px);
+        transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+        text-align: center;
     }
 </style>
